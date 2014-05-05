0116c01cdc543a60ed751e16692a854ab72b7105
Brian G. Tice <tice.physics@gmail.com>
2014-05-05 16:58:19 -0500

    add missing G4MagneticField.hh header

--- TrackExtrapolator/TabulatedField3D.hh
+++ TrackExtrapolator/TabulatedField3D.hh
@@ -34,6 +34,7 @@
 // $Id: PurgMagTabulatedField3D.hh,v 1.3 2006/06/29 16:06:05 gunter Exp $
 // GEANT4 tag $Name: geant4-09-01-patch-02 $
 
+#include "G4MagneticField.hh"
 #include <vector>
 
 class Settings;
@@ -41,8 +42,7 @@ class st_mysql;
 
 using std::vector;
 
-class TabulatedField3D: public G4MagneticField
-
+class TabulatedField3D : public G4MagneticField
 {
   // Storage space for the table
   vector< vector< vector< double > > > xField;
