00899dbc9793ca8181d8baa7381a25ad6edcd1aa
Brian G. Tice <tice.physics@gmail.com>
2014-06-05 14:47:12 -0500

    kTracker needs geometry files so check for them

--- setup.sh
+++ setup.sh
@@ -7,6 +7,22 @@ if [ ! -d "$KTRACKER_LIB" ]; then
   mkdir "$KTRACKER_LIB"
 fi
 
+#this package needs the geometry package.  check for it and try to find it
+if [ ! -d "$GEOMETRY_ROOT" ]; then
+  echo WARNING: You must setup the geometry package to run some kTracker programs.
+  if [ -d "$SEAQUEST_INSTALL_ROOT" ]; then
+    if [ -d "$SEAQUEST_INSTALL_ROOT/seaquest/geometry" ]; then
+      source $SEAQUEST_INSTALL_ROOT/seaquest/geometry/setup.sh
+      echo      OK, I found your geometry package at $GEOMETRY_ROOT
+    else
+      echo      The SeaQuest software distribution you are using does not have the geometry package installed
+    fi
+  else
+    echo          You do not appear to be using the SeaQuest software distribution so I cannot find this package for you
+    echo          Try 'source ../geometry/setup.sh'
+  fi
+fi
+
 #Set libs
 #remove existing references to kTracker
 if [ -f $SEAQUEST_SETUP_ROOT/setup_bash_utils.sh ]; then
