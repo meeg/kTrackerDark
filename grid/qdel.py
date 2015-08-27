8a0d4279dca9a21a912623d25debd716f935def9
Kun Liu <liuk.pku@gmail.com>
2015-08-27 14:50:03 -0600

    More fix and options in qdel.py

--- grid/qdel.py
+++ grid/qdel.py
@@ -10,6 +10,7 @@ from optparse import OptionParser
 parser = OptionParser('Usage: %prog [options]')
 parser.add_option('-r', '--range', type = 'string', dest = 'range', help = 'range of job ID, e.g. 101-202', default = '')
 parser.add_option('-p', '--pattern', type = 'string', dest = 'pattern', help = 'pattern of job name', default = '')
+parser.add_option('-s', '--status', type = 'string', dest = 'status', help = 'running status of jobs', default = '')
 (options, args) = parser.parse_args()
 
 if len(sys.argv) < 2:
@@ -24,12 +25,15 @@ pattern = '[a-z]'
 if options.pattern != '':
     pattern = options.pattern
 
+status = options.status
+
 output, err = subprocess.Popen('jobsub_q | grep liuk', stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True).communicate()
 jobDetails = []
 for line in output.split('\n'):
     vals = line.strip().split()
     jobID = int(re.findall(r'^(\d{7})', vals[0])[0])
-    jobDetails.append((jobID, vals[0], vals[8]))
+    jobDetails.append((jobID, vals[0], vals[8], vals[5]))
+jobDetails.sort(key = lambda x : x[0])
 
 toBeKilled = []
 for job in jobDetails:
@@ -37,8 +41,11 @@ for job in jobDetails:
         continue
     if len(re.findall(pattern, job[2])) == 0:
         continue
+    if status not in job[3]:
+        continue
 
     toBeKilled.append(job[1])
 
-for url in toBeKilled:
+for index, url in enumerate(toBeKilled):
+    print '%d/%d' % (index+1, len(toBeKilled)),
     os.system('jobsub_rm --jobid=%s')
