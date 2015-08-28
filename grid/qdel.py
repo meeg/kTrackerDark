0e8c1e3a70f92a987cc90e8f92f13448cbb37855
Kun Liu <liuk.pku@gmail.com>
2015-08-28 11:50:11 -0600

    qdel will ask user to confirm the list of jobs to be killed before actually do it

--- grid/qdel.py
+++ grid/qdel.py
@@ -11,6 +11,7 @@ parser = OptionParser('Usage: %prog [options]')
 parser.add_option('-r', '--range', type = 'string', dest = 'range', help = 'range of job ID, e.g. 101-202', default = '')
 parser.add_option('-p', '--pattern', type = 'string', dest = 'pattern', help = 'pattern of job name', default = '')
 parser.add_option('-s', '--status', type = 'string', dest = 'status', help = 'running status of jobs', default = '')
+parser.add_option('-d', '--debug', action = 'store_true', dest = 'debug', help = 'Enable massive debugging output', default = False)
 (options, args) = parser.parse_args()
 
 if len(sys.argv) < 2:
@@ -32,7 +33,7 @@ jobDetails = []
 for line in output.split('\n'):
     vals = line.strip().split()
     jobID = int(re.findall(r'^(\d{7})', vals[0])[0])
-    jobDetails.append((jobID, vals[0], vals[8], vals[5]))
+    jobDetails.append((jobID, vals[0], vals[8], vals[5], line))
 jobDetails.sort(key = lambda x : x[0])
 
 toBeKilled = []
@@ -44,7 +45,14 @@ for job in jobDetails:
     if status not in job[3]:
         continue
 
+    if options.debug:
+        print jobDetails[4]
     toBeKilled.append(job[1])
+
+if options.debug:
+    proceed = raw_input('Proceed (Y/N) ?')
+    if proceed.lower() == 'n':
+        sys.exit()
 
 for index, url in enumerate(toBeKilled):
     print '%d/%d' % (index+1, len(toBeKilled)),
